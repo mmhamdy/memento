26298155d9749dad45a8b21fa6b305f1e7a2fece
mmhamdy <mohamdy2772@gmail.com>
2022-04-07 07:10:04 +0200

    fix: static asset handling

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
-import background from './assets/images/background.jpg'
+import background from './assets/images/background.jpg?raw'
 
 export const globalStyles = css`
   body {
